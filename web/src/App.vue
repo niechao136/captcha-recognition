b4d249020e603e66f4e25f441b71cb1dbc4e7155
nie.chao <nie.chao@advantech.com.cn>
2024-05-20 17:49:01 +0800

    web

---
+++ web/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ElIcon, ElUpload } from 'element-plus'
+import { UploadFilled } from '@element-plus/icons-vue'
+</script>
+
+<template>
+  <el-upload drag>
+    <el-icon><upload-filled/></el-icon>
+  </el-upload>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ web/vite.config.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import ElementPlus from 'unplugin-element-plus/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    ElementPlus({}),
+    vue(),
+    VueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
